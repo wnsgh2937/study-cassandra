13161fc47f33185b3ff0b3fb0d9efc66a5d08a3a
kakaoent-leo <leo.ee@kakaoent.com>
2024-03-11 20:46:59 +0900

    INIT

---
+++ src/main/kotlin/org/example/studycassandra/StudyCassandraApplication.kt
@@ -0,0 +1,11 @@
+package org.example.studycassandra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyCassandraApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyCassandraApplication>(*args)
+}
